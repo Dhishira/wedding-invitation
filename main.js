c684ffb0c516432c62356c81cdfcdf42762be984
Dhishira <84129757+Dhishira@users.noreply.github.com>
2022-02-10 20:05:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400 ,350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video,0,0,550,480);
+}
+
+function take_snapshot()
+{
+    save("myFilterImage.png");
+}
